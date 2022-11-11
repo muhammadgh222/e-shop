44ab3e9531d467bf186cba25e61f711d56764f30
devdavinci <dev.muhammad222000@gmail.com>
2022-11-11 06:30:29 +0200

    reviews aggregation

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -26,6 +26,38 @@ const reviewSchema = new mongoose.Schema({
 
 reviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
+reviewSchema.statics.calculateAverageRating = async function (productId) {
+  const result = await this.aggregate([
+    { $match: { product: productId } },
+    {
+      $group: {
+        _id: null,
+        ratingsAvg: { $avg: "$rating" },
+        ratingsQuantity: { $sum: 1 },
+      },
+    },
+  ]);
+
+  try {
+    await this.model("Product").findOneAndUpdate(
+      { _id: productId },
+      {
+        ratingsAvg: Math.ceil(result[0]?.ratingsAvg || 0),
+        ratingsQuantity: result[0]?.ratingsQuantity || 0,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+reviewSchema.post("save", async function () {
+  await this.constructor.calculateAverageRating(this.product);
+});
+
+reviewSchema.post("remove", async function () {
+  await this.constructor.calculateAverageRating(this.product);
+});
+
 const Review = mongoose.model("Review", reviewSchema);
 
 module.exports = Review;
